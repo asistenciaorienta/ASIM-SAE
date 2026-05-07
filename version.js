0f51feee05cb101a28aec79acc28f7a7d6c4a009
asistenciaorienta <asistenciaorienta.gr.sae@juntadeandalucia.es>
2026-05-07 11:03:04 +0200

    Update version.js

--- version.js
+++ version.js
@@ -1,4 +1,4 @@
 // config.js
 const APP_CONFIG = {
-  version: "Versión pruebas 28-04-2026 — Servicio Andaluz de Empleo - Junta de Andalucía"
+  version: "Versión pruebas 07-05-2026 — Servicio Andaluz de Empleo - Junta de Andalucía"
 };
